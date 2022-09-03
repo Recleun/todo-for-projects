601bd6524794d7e55c44cf309fd8ba12029dffa5
Trashie <98621222+TrashieL0L@users.noreply.github.com>
2022-09-04 01:13:15 +0200

    Optimized list and create, added remove and set commands

--- ToDo for Projects/data/main.py
+++ ToDo for Projects/data/main.py
@@ -29,8 +29,14 @@ def save(toSave):
 		file.write(jObj)
 
 def lst():
-	data = load()
-	click.echo(data['projects'])
+	data = load()['projects']
+	click.echo('\n')
+	click.echo(f'Projects ({len(data)}):')
+	if len(data) > 0:
+		for i in data:
+			click.echo(' - {}'.format(i['name']))
+	else:
+		click.echo(' - No projects found')
 
 def create(desc, name):
 	data = load()
@@ -40,5 +46,25 @@ def create(desc, name):
 			click.echo('\n [FAILED] A project with the same name already exists.')
 			return
 	projects.append({"name": name, "description": desc, "tasks": []})
-	print(projects)
+	click.echo('\n [SUCCESS] Added project')
 	save(projects)
+
+def remove(name):
+	data = load()
+	projects = data['projects']
+	index = 0
+	success = False
+	for project in projects:
+		if project['name'] == name:
+			projects.pop(index)
+			success = True
+			save(projects)
+			click.echo('\n [SUCCESS] Removed project')
+		index += 1
+	if success:
+		pass
+	else:
+		click.echo('\n [FAILED] Project not found')
+
+def set(name, type, new):
+	pass

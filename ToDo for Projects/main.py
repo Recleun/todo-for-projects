abdbc2374c0ac687003ce1654996cc09ebfbc44b
Trashie <98621222+TrashieL0L@users.noreply.github.com>
2022-09-04 01:12:41 +0200

    Created remove and set commands

--- ToDo for Projects/main.py
+++ ToDo for Projects/main.py
@@ -14,3 +14,15 @@ def lst():
 @click.argument('name')
 def create(desc, name):
 	data.create(desc, name)
+
+@cli.command(name='remove', help='Removes a project')
+@click.argument('name')
+def remove(name):
+	data.remove(name)
+
+@cli.command(name='set', help='<project name> <type: name/description> <new name/description>')
+@click.argument('project name')
+@click.argument('type')
+@click.argument('new')
+def set(name, type, new):
+	data.set(name, type, new)

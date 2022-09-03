79375bbe147dcd9a8bc92f88cd76e710b2370c3e
Trashie <98621222+TrashieL0L@users.noreply.github.com>
2022-09-04 00:06:39 +0200

    Added create command and save function

--- ToDo for Projects/data/main.py
+++ ToDo for Projects/data/main.py
@@ -1,8 +1,9 @@
 import click
 import json
 
+filePath = './data/projects_data.json'
+
 def load():
-	filePath = './data/projects_data.json'
 	try:
 		file = open(filePath, 'r+')
 		return json.load(file)
@@ -10,7 +11,7 @@ def load():
 		create = open(filePath, 'w+')
 
 		toWrite = {
-			"tasks": []
+			"projects": []
 		}
 
 		json.dump(toWrite, create)
@@ -19,6 +20,25 @@ def load():
 		file = open(filePath, 'r+')
 		return json.load(file)
 
+def save(toSave):
+	toWrite = {
+		"projects": toSave
+	}
+	jObj = json.dumps(toWrite, indent=4)
+	with open(filePath, 'w') as file:
+		file.write(jObj)
+
 def lst():
 	data = load()
-	click.echo(data['tasks'])
+	click.echo(data['projects'])
+
+def create(desc, name):
+	data = load()
+	projects = data['projects']
+	for project in projects:
+		if project['name'] == name:
+			click.echo('\n [FAILED] A project with the same name already exists.')
+			return
+	projects.append({"name": name, "description": desc, "tasks": []})
+	print(projects)
+	save(projects)
